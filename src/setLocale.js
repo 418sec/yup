bee819d276d35cf3d56adf64f9656983e9214b09
d3v53c <shitfi63@gmail.com>
2020-10-28 09:22:42 -0700

    changed continue to return

--- src/setLocale.js
+++ src/setLocale.js
@@ -3,7 +3,7 @@ import locale from './locale';
 export default function setLocale(custom) {
   Object.keys(custom).forEach(type => {
     if (type === '__proto__') {
-      continue;
+      return;
     }
 
     Object.keys(custom[type]).forEach(method => {
