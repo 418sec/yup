d45d3b84751a006a52b9525753d1083eab7232cf
d3v53c <shitfi63@gmail.com>
2020-10-28 08:09:09 -0700

    Fix Prototype pollution

--- src/setLocale.js
+++ src/setLocale.js
@@ -2,7 +2,12 @@ import locale from './locale';
 
 export default function setLocale(custom) {
   Object.keys(custom).forEach(type => {
+    if (type === '__proto__' || type === 'constructor' || type === 'prototype') {
+      throw new Error('Prototype pollution attempt detected.');
+    }
+
     Object.keys(custom[type]).forEach(method => {
+      console.log({ type, method });
       locale[type][method] = custom[type][method];
     });
   });
