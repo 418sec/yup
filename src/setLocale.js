5fb6cc1d19768705e2a7be1010f9a2cbac953d36
d3v53c <shitfi63@gmail.com>
2020-10-28 08:35:48 -0700

    Instead of throwing exception, will skip blacklisted keys

--- src/setLocale.js
+++ src/setLocale.js
@@ -3,7 +3,7 @@ import locale from './locale';
 export default function setLocale(custom) {
   Object.keys(custom).forEach(type => {
     if (type === '__proto__' || type === 'constructor' || type === 'prototype') {
-      throw new Error('Prototype pollution attempt detected.');
+      continue;
     }
 
     Object.keys(custom[type]).forEach(method => {
